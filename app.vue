2889bcbd249b3201b159ed193fb423aa8af40a07
Dmytro <dimores95@gmail.com>
2022-11-10 16:26:51 +0200

    added name search

--- app.vue
+++ app.vue
@@ -6,29 +6,108 @@
       <div class="option-container">
         <h4>1) Choose a gender</h4>
         <div class="option-buttons">
-          <button class="option option-left">Boy</button>
-          <button class="option">Unisex</button>
-          <button class="option option-right">Girl</button>
+          <button
+            class="option option-left"
+            :class="options.gender === Gender.BOY && 'option-active'"
+            @click="options.gender = Gender.BOY"
+          >
+            Boy
+          </button>
+          <button
+            class="option"
+            :class="options.gender === Gender.UNISEX && 'option-active'"
+            @click="options.gender = Gender.UNISEX"
+          >
+            Unisex
+          </button>
+          <button
+            class="option option-right"
+            :class="options.gender === Gender.GIRL && 'option-active'"
+            @click="options.gender = Gender.GIRL"
+          >
+            Girl
+          </button>
         </div>
       </div>
       <div class="option-container">
         <h4>2) Choose the name`s popularity</h4>
         <div class="option-buttons">
-          <button class="option option-left">Trendy</button>
-          <button class="option option-right">Unique</button>
+          <button
+            class="option option-left"
+            :class="options.popularity === Popularity.TRENDY && 'option-active'"
+            @click="options.popularity = Popularity.TRENDY"
+          >
+            Trendy
+          </button>
+          <button
+            class="option option-right"
+            :class="options.popularity === Popularity.UNIQUE && 'option-active'"
+            @click="options.popularity = Popularity.UNIQUE"
+          >
+            Unique
+          </button>
         </div>
       </div>
       <div class="option-container">
         <h4>2) Choose names` length`</h4>
         <div class="option-buttons">
-          <button class="option option-left">Long</button>
-          <button class="option">All</button>
-          <button class="option option-right">Shoort</button>
+          <button
+            class="option option-left"
+            :class="options.length === Length.LONG && 'option-active'"
+            @click="options.length = Length.LONG"
+          >
+            Long
+          </button>
+          <button
+            class="option"
+            :class="options.length === Length.ALL && 'option-active'"
+            @click="options.length = Length.ALL"
+          >
+            All
+          </button>
+          <button
+            class="option option-right"
+            :class="options.length === Length.SHORT && 'option-active'"
+            @click="options.length = Length.SHORT"
+          >
+            Short
+          </button>
         </div>
       </div>
+      <button class="primary" @click="computeSelectedNames">Find Names</button>
     </div>
+    {{ selectedNames }}
   </div>
 </template>
+
+<script setup lang="ts">
+import { Gender, Length, Popularity, names } from "@/data";
+interface OptionState {
+  gender: Gender;
+  popularity: Popularity;
+  length: Length;
+}
+
+const options = reactive<OptionState>({
+  gender: Gender.GIRL,
+  length: Length.ALL,
+  popularity: Popularity.TRENDY
+});
+
+const computeSelectedNames = () => {
+  const filteredNames = names
+    .filter((name) => name.gender === options.gender)
+    .filter((name) => name.popularity === options.popularity)
+    .filter((name) => {
+      if (options.length === Length.ALL) return true;
+      else return name.length === options.length;
+    });
+
+  selectedNames.value = filteredNames.map((name) => name.name);
+};
+
+const selectedNames = ref<string[]>([]);
+</script>
 
 <style scoped>
 .container {
@@ -69,5 +148,20 @@
 }
 .option-right {
   border-radius: 0 1rem 1rem 0;
+}
+
+.option-active {
+  background: rgb(249, 87, 89);
+  color: white;
+}
+.primary {
+  background-color: rgb(249, 87, 89);
+  color: white;
+  border-radius: 6.5rem;
+  border: none;
+  padding: 0.75rem 4rem;
+  font-size: 1rem;
+  margin-top: 1rem;
+  cursor: pointer;
 }
 </style>
