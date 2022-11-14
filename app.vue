daa94ad54d572d0ad9bb8d9123583b11ef01b447
Dmytro <dimores95@gmail.com>
2022-11-14 15:07:37 +0200

    finished

--- app.vue
+++ app.vue
@@ -12,10 +12,17 @@
       <button class="primary" @click="computeSelectedNames">Find Names</button>
     </div>
     <div class="cards-container">
-      <div v-for="name in selectedNames" :key="name" class="card">
+      <!-- <div v-for="name in selectedNames" :key="name" class="card">
         <h4>{{ name }}</h4>
         <p>X</p>
-      </div>
+      </div> -->
+      <CardName
+        v-for="(name, index) in selectedNames"
+        :key="name"
+        :name="name"
+        @remove="() => removeName(index)"
+        :index="index"
+      />
     </div>
   </div>
 </template>
@@ -47,6 +54,12 @@ const computeSelectedNames = () => {
 };
 
 const selectedNames = ref<string[]>([]);
+
+const removeName = (index: number) => {
+  const filteredNames = [...selectedNames.value];
+  filteredNames.splice(index, 1);
+  selectedNames.value = filteredNames;
+};
 
 const optionArray = [
   {
@@ -101,23 +114,5 @@ const optionArray = [
   display: flex;
   flex-wrap: wrap;
   margin: 3rem;
-}
-.card {
-  background-color: rgb(27, 60, 138);
-  width: 28%;
-  color: white;
-  border-radius: 1rem;
-  padding: 0.1rem;
-  margin-right: 0.5rem;
-  margin-bottom: 1rem;
-  position: relative;
-}
-
-.card p {
-  position: absolute;
-  top: -29%;
-  left: 92.5%;
-  cursor: pointer;
-  color: rgba(255, 255, 255, 0.178);
 }
 </style>
